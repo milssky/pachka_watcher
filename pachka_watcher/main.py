bc1abd343b7fd6132d2f581019f47ded777dcbb3
Alexey Potapov <2furius@gmail.com>
2024-10-16 22:08:17 +0300

    Run fixes

--- pachka_watcher/main.py
+++ pachka_watcher/main.py
@@ -1,2 +1,20 @@
+import asyncio
+
+from pachka_watcher.integrations.client import AsyncPachkaClient
+from pachka_watcher.settings import settings
+
+
+async def run():
+    client = AsyncPachkaClient(settings)
+    result = await client.get(
+        'https://api.pachca.com/api/shared/v1/chats'
+    )
+    print(result.json())
+
+
 def main():
-    pass
+    asyncio.run(run())
+
+
+if __name__ == '__main__':
+    main()
